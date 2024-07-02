e5b6c9f195a65e4755824694cd3e6b0fb51d7e7b
wilfried <wilfried.odorico@gmail.com>
2024-07-02 21:35:55 +0200

    change name file tournament and player

---
+++ models/tournament_model.py
@@ -0,0 +1,26 @@
+import datetime
+from typing import List
+
+from models.player_model import Player
+from models.round_model import Round
+
+
+class Tournament:
+    def __init__(
+        self,
+        name: str,
+        location: str,
+        description: str,
+        date_start: datetime,
+        date_end: datetime,
+    ):
+        self.name = name
+        self.location = location
+        self.description = description
+        self.date_start = date_start
+        self.date_end = date_end
+
+        self.number_of_round: int = 4
+        self.number_of_current_round: int = 1
+        self.rounds: List[Round] = []
+        self.players: List[Player] = []
