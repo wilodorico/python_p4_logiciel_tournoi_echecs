424f0f261fbed42e2a65c44821faced1668eb5ab
wilfried <wilfried.odorico@gmail.com>
2024-07-19 09:55:38 +0200

    Add method run, method create_tournament + add TournamentManager + method create_tournament and to_dict in model tournament

--- controllers/tournament_controller.py
+++ controllers/tournament_controller.py
@@ -1,32 +1,4 @@
-# Creer le tournoi
-
-# tournament = ...
-# self.name = name
-#         self.location = location
-#         self.description = description
-#         self.date_start = date_start
-#         self.date_end = date_end
-
-#         self.number_of_round: int = 4
-#         self.number_of_current_round: int = 1
-#         self.rounds: List[Round] = []
-#         self.players: List[Player] = []
-
-
-# Ajouter les players au tournoi
-# Tournament.add_player(player_id=1)
-# Tournament.add_player(player_id=2)
-# Tournament.add_player(player_id=3)
-# Tournament.add_player(player_id=4)
-
-
-# Créer Round
-#
-# Tournament.create_round()  -> id de round 1
-
-# attente de resultats
-
-from models.tournament_model import Tournament
+from models.tournament_model import TournamentManager
 from views.tournament_view import TournamentView
 
 
@@ -34,15 +6,33 @@ class TournamentController:
 
     def __init__(self):
         self.tournament_view = TournamentView()
+        self.tournament_manager = TournamentManager()
+
+    def run(self):
+        while True:
+            self.tournament_view.display_tournament_menu()
+            choice: int = self.tournament_view.request_user_choice()
+
+            # remplacer par le match case
+
+            if choice == 1:
+                # self.show_tournaments()
+                pass
+            if choice == 2:
+                self.create_tournament()
+            if choice == 3:
+                # self.add_player_to_tournament()
+                pass
+            if choice == 4:
+                pass
+            if choice == 5:
+                break
 
     def create_tournament(self):
         name, location, description, date_start, date_end = (
             self.tournament_view.get_tournament_info()
         )
-        tournament = Tournament(name, location, description, date_start, date_end)
 
-        self.tournament_view.display_tournament_info(tournament)
-
-        self.tournament_view.display_tournament_menu()
-
-        print(tournament)
+        self.tournament_manager.create_tournament(
+            name, location, description, date_start, date_end
+        )
--- models/tournament_model.py
+++ models/tournament_model.py
@@ -1,6 +1,8 @@
 import datetime
 from typing import List
 
+from tinydb import TinyDB
+
 from models.player_model import Player
 from models.round_model import Round
 
@@ -13,7 +15,9 @@ class Tournament:
         description: str,
         date_start: datetime,
         date_end: datetime,
+        id: str = None,
     ):
+        self.id = id
         self.name = name
         self.location = location
         self.description = description
@@ -24,3 +28,43 @@ class Tournament:
         self.number_of_current_round: int = 1
         self.rounds: List[Round] = []
         self.players: List[Player] = []
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "location": self.location,
+            "description": self.description,
+            "date_start": self.date_start,
+            "date_end": self.date_end,
+            "number_of_round": self.number_of_round,
+            "number_of_current_round": self.number_of_current_round,
+            "rounds": self.rounds,
+            "players": self.players,
+        }
+
+
+class TournamentManager:
+    def __init__(self, db_path="data/tournaments/tournaments.json"):
+        self.db = TinyDB(db_path, indent=4)
+        self.tournaments_table = self.db.table("tournaments")
+
+    def create_tournament(
+        self,
+        name: str,
+        location: str,
+        description: str,
+        date_start: datetime,
+        date_end: datetime,
+    ):
+        new_tournament = Tournament(
+            name,
+            location,
+            description,
+            date_start.strftime("%d-%m-%Y"),
+            date_end.strftime("%d-%m-%Y"),
+        )
+
+        self.tournaments_table.insert(new_tournament.to_dict())
+
+        print("Tournoi enregistré avec succès !")
