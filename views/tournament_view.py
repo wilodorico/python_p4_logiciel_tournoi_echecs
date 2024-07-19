3091b580998603619da1134f0450d00c40523881
wilfried <wilfried.odorico@gmail.com>
2024-07-19 09:04:03 +0200

    Add fonction validation on get_tournament_info + change tournament menu + add method display_tournaments

--- views/tournament_view.py
+++ views/tournament_view.py
@@ -1,14 +1,17 @@
+from utils.input_validation import get_non_empty_input, get_valid_date_format
+
+
 class TournamentView:
 
     def get_tournament_info(self):
         print()
         print("Renseignez les informations du tournoi.")
         print()
-        name = input("Veuillez entrer le nom : ")
-        location = input("Veuillez entrer le lieu : ")
-        description = input("Veuillez entrer la description : ")
-        date_start = input("Veuillez saisir la date de début : ")
-        date_end = input("Veuillez saisir la date de fin : ")
+        name = get_non_empty_input("Veuillez entrer le nom : ")
+        location = get_non_empty_input("Veuillez entrer le lieu : ")
+        description = get_non_empty_input("Veuillez entrer la description : ")
+        date_start = get_valid_date_format("Veuillez saisir la date de début : ")
+        date_end = get_valid_date_format("Veuillez saisir la date de fin : ")
 
         return name, location, description, date_start, date_end
 
@@ -20,19 +23,36 @@ class TournamentView:
 
     def display_tournament_menu(self):
         print()
-        print("1. Ajouter les joueurs")
-        print("2. Créer le Tour")
+        print("1. Liste des tournois")
+        print("2. Créer un tournoi")
+        print("3. Ajouter les joueurs au tournoi")
+        print("4. Créer un tour")
+        print("5. Retour au menu principal")
 
+    def request_user_choice(self):
         while True:
-            choice = input("Veuillez entrer un choix (1 ou 2) ")
+            print()
+            choice = input("Veuillez entrer un choix (1, 2, 3, 4 ou 5) ")
             print()
             try:
                 choice_number: int = int(choice)
-                if choice_number in [1, 2]:
+                if choice_number in [1, 2, 3, 4, 5]:
                     return choice_number
                 else:
-                    print("Choix invalide : Veuillez entrer 1 ou 2")
+                    print("Choix invalide : Veuillez entrer 1, 2, 3, 4 ou 5")
                     print()
             except ValueError:
-                print("Erreur : Veuillez entrer un nombre (1 ou 2)")
+                print("Erreur : Veuillez entrer un nombre (1, 2, 3, 4 ou 5)")
                 print()
+
+    def display_tournaments(self, tournaments):
+        print("Liste des tournois enregistrés.")
+        print(
+            "-------------------------------------------------------------------------------"
+        )
+        for tournament in tournaments:
+            print(tournament)
+
+        print(
+            "-------------------------------------------------------------------------------"
+        )
