2b6f070edba7802d0aedb9765ff5ef3999298ba2
wilfried <wilfried.odorico@gmail.com>
2024-07-25 17:57:51 +0200

    change if condition to match case and add method managing_tournament

--- controllers/tournament_controller.py
+++ controllers/tournament_controller.py
@@ -13,19 +13,15 @@ class TournamentController:
             self.tournament_view.display_tournament_menu()
             choice: int = self.tournament_view.request_user_choice()
 
-            # remplace par le match case
-
-            if choice == 1:
-                self.show_tournaments()
-            if choice == 2:
-                self.create_tournament()
-            if choice == 3:
-                # self.add_player_to_tournament()
-                pass
-            if choice == 4:
-                pass
-            if choice == 5:
-                break
+            match choice:
+                case 1:
+                    self.show_tournaments()
+                case 2:
+                    self.create_tournament()
+                case 3:
+                    self.managing_last_tournament()
+                case 4:
+                    break
 
     def create_tournament(self):
         name, location, description, date_start, date_end = (
@@ -39,3 +35,21 @@ class TournamentController:
     def show_tournaments(self):
         tournaments = self.tournament_manager.get_tournaments()
         self.tournament_view.display_tournaments(tournaments)
+
+    def managing_tournament(self):
+        while True:
+            self.tournament_view.display_tournament_management_menu()
+            choice: int = self.tournament_view.request_tournament_management_choice()
+
+            match choice:
+                case 1:
+                    # self.add_players_to_tournament()
+                    print("TODO")
+                case 2:
+                    # self.show_players_of_tournament()
+                    print("TODO 2")
+                case 3:
+                    # self.managing_rounds_of_tournament()
+                    print("TODO 3")
+                case 4:
+                    break
