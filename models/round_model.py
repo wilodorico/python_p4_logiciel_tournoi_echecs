c05c7eb127780c55000603e9fa3074d254b187ed
wilfried <wilfried.odorico@gmail.com>
2024-08-29 19:11:28 +0200

    Change menu round and add generate_matches and create_random_matches

--- models/round_model.py
+++ models/round_model.py
@@ -1,7 +1,11 @@
-import datetime
+import random
+from datetime import datetime
 from enum import Enum
-
+from typing import List
 from tinydb import TinyDB
+
+from models.player_model import Player
+from models.match_model import Match
 
 
 class RoundStatus(Enum):
@@ -13,7 +17,7 @@ class RoundStatus(Enum):
 class Round:
     def __init__(self, name: str):
         self.name = name
-        self.matchs: list = []
+        self.matches: list = []
         self.start_at: datetime = None
         self.end_at: datetime = None
         self.status: RoundStatus = RoundStatus.CREATED
@@ -24,9 +28,9 @@ class Round:
     def to_dict(self):
         return {
             "name": self.name,
-            "matchs": self.matchs,
-            "start_at": self.start_at.strftime("%d-%m-%Y") if self.start_at else None,
-            "end_at": self.end_at.strftime("%d-%m-%Y") if self.end_at else None,
+            "matches": self.matches,
+            "start_at": self.start_at.strftime("%d-%m-%Y %H:%M:%S") if self.start_at else None,
+            "end_at": self.end_at.strftime("%d-%m-%Y %H:%M:%S") if self.end_at else None,
             "status": self.status.value,
         }
 
@@ -37,45 +41,103 @@ class Round:
         self.end_at = datetime.now()
 
     def is_running(self):
-        if self.start_at:
-            return True
-        return False
+        return self.start_at
+
+    def add_match(self, match: Match):
+        self.matches.append(match.players)
 
 
 class RoundManager:
     def __init__(self, db_path="data/tournaments/tournaments.json"):
         self.db = TinyDB(db_path, indent=4, ensure_ascii=False, encoding="utf-8")
         self.tournaments_table = self.db.table("tournaments")
-
-    def create_round(self, tournament_id):
-        tournament = self.tournaments_table.get(doc_id=tournament_id)
-        total_tournament_rounds = len(tournament["rounds"])
-        current_round = tournament["number_of_current_round"]
-
-        if tournament["rounds"]:
-            if (
-                tournament["rounds"][-1]["status"] == RoundStatus.CREATED.value
-                or tournament["rounds"][-1]["status"] == RoundStatus.STARTED.value
-            ):
-                return print(f"Tour n°{total_tournament_rounds} déjà créée ou en cours")
-
-        total_tournament_rounds += 1
-        new_round = Round(f"Tour n°{total_tournament_rounds}")
-        tournament["rounds"].append(new_round.to_dict())
-        self.tournaments_table.update(tournament, doc_ids=[tournament_id])
-
-        print(f"Round '{new_round.name}' enregistré avec succès !")
+        self.matches = []
 
     def start_round(self, tournament_id):
         tournament = self.tournaments_table.get(doc_id=tournament_id)
         if tournament["rounds"]:
             current_round = tournament["rounds"][-1]
         else:
-            return print("Aucun tour créé: Veuillez d'abord créer un tour")
+            return print("Aucun Round créé: Veuillez d'abord créer un Round")
 
         if current_round["status"] == RoundStatus.CREATED.value:
             current_round["status"] = RoundStatus.STARTED.value
+            current_round["start_at"] = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
             self.tournaments_table.update(tournament, doc_ids=[tournament_id])
             return print(f"{current_round['name']} démarrée avec succès !")
         else:
             return print(f"{current_round['name']} déjà en cours")
+
+    def create_round(self, tournament_id):
+        tournament = self.tournaments_table.get(doc_id=tournament_id)
+        if not tournament:
+            print(f"Aucun tournoi trouvé avec l'ID: {tournament_id}")
+            return
+
+        total_tournament_rounds = len(tournament["rounds"])
+
+        if tournament["rounds"]:
+            if (
+                tournament["rounds"][-1]["status"] == RoundStatus.CREATED.value
+                or tournament["rounds"][-1]["status"] == RoundStatus.STARTED.value
+            ):
+                return print(f"Round n°{total_tournament_rounds} déjà créée ou en cours")
+
+        total_tournament_rounds += 1
+        tournament["number_of_current_round"] = total_tournament_rounds
+        new_round = Round(f"Round {total_tournament_rounds}")
+        tournament["rounds"].append(new_round.to_dict())
+        self.tournaments_table.update(tournament, doc_ids=[tournament_id])
+
+        print(f"{new_round.name} enregistré avec succès !")
+
+    def generate_matches(self, tournament_id):
+        tournament = self.tournaments_table.get(doc_id=tournament_id)
+        if not tournament:
+            print(f"Aucun tournoi trouvé avec l'ID: {tournament_id}")
+            return
+
+        players: List[Player] = tournament.get("players", [])
+        max_players = tournament.get("max_players")
+
+        if len(players) < max_players:
+            print(f"Veuillez enregistrer les {max_players} joueurs avant de créer des matchs")
+            return
+
+        current_round_number = len(tournament["rounds"])
+
+        if current_round_number == 1:
+            if (
+                not tournament["rounds"][-1]["matches"]
+                and tournament["rounds"][-1]["status"] == RoundStatus.STARTED.value
+            ):
+                self.matches = self.create_random_matches(players)
+            else:
+                return print("Les matchs sont déjà générés, terminez le Round en saisissant les scores")
+        else:
+            print("TODO")
+
+        tournament["rounds"][-1]["matches"] = self.matches
+        self.tournaments_table.update(tournament, doc_ids=[tournament_id])
+
+        print("Les matchs ont été générés avec succès !")
+
+    def create_random_matches(self, players: List[Player]):
+        matches = []
+        random.shuffle(players)
+
+        for i in range(0, len(players) - 1, 2):
+            player1 = players[i]
+            player2 = players[i + 1]
+            match = Match(
+                player1["firstname"] + " " + player1["lastname"],
+                0.0,
+                player2["firstname"] + " " + player2["lastname"],
+                0.0,
+            )
+            matches.append(match.players)
+
+        if len(players) % 2 != 0:
+            print(f"Le joueur {players[-1]['firstname']} {players[-1]['lastname']} n'a pas d'adversaire pour ce round")
+
+        return matches
--- views/round_view.py
+++ views/round_view.py
@@ -4,20 +4,20 @@ class RoundView:
         print("-----------------------------")
         print("Menu Round : ")
         print("-----------------------------")
-        print(f"1. Créer le tour n°{round_number}")
-        print("2. Démarrer le round")
-        print("3. Terminer le round")
+        print(f"1. Démarrer le Round n°{round_number}")
+        print("2. Afficher la liste des matchs")
+        print("3. Terminer le Round")
         print("4. Renseigner les scores")
         print("5. Retour au menu du tournois")
         print()
 
     def request_user_choice(self) -> int:
         while True:
-            choice = input("Veuillez entrer un choix (1, 2, 3, 4 ou 5): ")
+            choice = input("Veuillez entrer un choix (1, 2, 3, 4 ou 5) : ")
             print()
             try:
                 choice_number: int = int(choice)
-                if choice_number in [1, 2, 3, 4, 5]:
+                if choice_number in [1, 2, 3, 4]:
                     return choice_number
                 else:
                     print("Choix invalide : Veuillez entrer 1, 2, 3, 4 ou 5")
