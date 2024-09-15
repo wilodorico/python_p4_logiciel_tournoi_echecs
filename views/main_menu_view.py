98536a06584eda69e541b073cb16c7ea3f8a766b
wilfried <wilfried.odorico@gmail.com>
2024-09-15 11:29:15 +0200

    Add docstrings and add constructor for var console

--- views/main_menu_view.py
+++ views/main_menu_view.py
@@ -1,10 +1,14 @@
 from rich.console import Console
 from rich.prompt import IntPrompt
+
 from utils.rich_component import alert_message
 
 
 class MainMenuView:
-    console = Console()
+    """Class to display the main menu."""
+
+    def __init__(self):
+        self.console = Console()
 
     def display_main_menu(self):
         self.console.print("=========================", style="deep_sky_blue1")
@@ -17,6 +21,9 @@ class MainMenuView:
         self.console.print("=========================", style="deep_sky_blue1")
 
     def request_user_choice(self) -> int:
+        """Requests the user choice from the console.
+        Returns the choice as an integer.
+        """
         while True:
             try:
                 choice: int = IntPrompt.ask("Veuillez entrer un choix", choices=["1", "2", "3", "4"])
